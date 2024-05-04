88765affd4cd59f088d74de2fc9d795bb7ffdbb0
gtmcris <gouthamsuraj2003@gmail.com>
2024-05-04 16:55:03 +0530

    Initial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+        Console.WriteLine($"{i} - FizzBuzz");
+    }
+
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} - Fizz");
+    }
+
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+}
